82707a3aa3db6caf993136ba0efac033573431c8
TranLocDev <loc.tran@vn.innova.com>
2025-12-12 15:34:08 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/message", (req, res) => {
+    const date = new Date();
+    const vnTime = date.toLocaleString('vi-VN', { timeZone: 'Asia/Ho_Chi_Minh' });
+    res.json({ message: `Hello World ${vnTime}` });
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
